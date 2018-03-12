43568bf7f048f651df5f285c9de5c3402763d9ed
Noah Tatko <ntat.taco@gmail.com>
2018-03-12 13:10:34 -0500

    added data validation

--- exercise 3-1.js
+++ exercise 3-1.js
@@ -1,7 +1,12 @@
 function min(a, b) {
-  if (a > b) {
-    return b;
+  if (typeof a === 'number' && typeof b === 'number'){
+    if (a > b) {
+      return b;
+    } else {
+      return a;
+    }
   } else {
-    return a; 
+    console.log("Not a number");
+    return -1;
   }
 }
--- exercise 3-2.js
+++ exercise 3-2.js
@@ -1,11 +1,16 @@
 function isEven(a) {
-  if (a == 0) {
-    return Boolean(1);
-  } else if (a == 1) {
-    return Boolean(-0);
-  } else if (a < 0){
-    isEven(a + 2);
+  if(typeof a === 'number'){
+    if (a == 0) {
+      return Boolean(1);
+    } else if (a == 1) {
+      return Boolean(-0);
+    } else if (a < 0){
+      isEven(a + 2);
+    } else {
+      isEven(a - 2);
+    }
   } else {
-    isEven(a - 2);
+    console.log("Not a number");
+    return -1;
   }
 }
