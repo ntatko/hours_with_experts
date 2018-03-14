b1b074ea27eadab55c46ee1bdae66e1a5a2f9ae4
Noah Tatko <ntat.taco@gmail.com>
2018-03-14 18:52:13 -0500

    stupid error...

--- exercise 2-2.js
+++ exercise 2-2.js
@@ -1,7 +1,7 @@
 let number = 0;
 while (number < 100) {
   number += 1;
-  if (number % 3 == 0 && number % 5) {
+  if (number % 3 == 0 && number % 5 == 0) {
     console.log("FizzBuzz");
   } else if (number % 3 == 0) {
     console.log("Fizz");
