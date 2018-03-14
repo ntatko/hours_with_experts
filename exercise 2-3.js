c1e033109865ff7f6717b1cab923ef289687472f
Noah Tatko <ntat.taco@gmail.com>
2018-03-14 18:59:32 -0500

    an update to remove hanging space

--- exercise 2-3.js
+++ exercise 2-3.js
@@ -1,8 +1,9 @@
 let size = 8;
 let string = "";
-for (let i = 0; i < size; i++) {
+for (let i = 0; i < size-1; i++) {
   string += "# ";
 }
+string += "#";
 for (let i = 0; i < size; i++) {
   if (i % 2 == 0) {
     console.log(" " + string);
