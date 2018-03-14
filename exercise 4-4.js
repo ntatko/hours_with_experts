42ed22b38f4f713b2f1cfb7804ec83391a7fe692
Noah Tatko <ntat.taco@gmail.com>
2018-03-14 08:46:48 -0500

    Finished 4-4, but still don't quite know if it's correct. Need to test chapter 4 exercises to see if they actually work.

--- exercise 4-4.js
+++ exercise 4-4.js
@@ -7,7 +7,7 @@ function deepEqual(obj1, obj2) {
       if (obj2.value) {
         // obj2 is a list
         let compared = Boolean(true);
-        while (obj1.rest != null && obj2.rest != null) {
+        while (obj1.value && obj2.value) {
           if (obj1.value != obj2.value) {
             compared = Boolean(false);
           }
@@ -16,18 +16,20 @@ function deepEqual(obj1, obj2) {
         }
         return compared;
       } else {
-        // obj2 is a list
+        // obj2 is not a list
         if (Array.isArray(obj2)) {
           let compared = Boolean(true);
           for (let i = 0; i < obj2.length; i++) {
-            if (obj1.value != obj2[i]) {
+            if (!obj.value) {
+              break;
+            } else if (obj1.value != obj2[i]) {
               compared = Boolean(false);
             }
             obj1 = obj1.value;
           }
           return compared;
         } else {
-          console.log("Hmm. They're not equal?");
+          console.log("Hmm. They're not equal? (1)"); //debug only
           return Boolean(false);
         }
       }
@@ -36,21 +38,21 @@ function deepEqual(obj1, obj2) {
         // obj2 is a list
         if (Array.isArray(obj1)) {
           for (let i = 0; i < obj1.length; i++) {
-
+            if (!obj.value) {
+              break;
+            }
           }
         } else {
-          console.log("Hmm. They're not equal?");
+          console.log("Hmm. They're not equal? (2)"); //debug only
           return Boolean(false);
         }
 
       } else {
         // obj2 is not a list
-        if (Array.isArray(obj2)){
-
-        } else {
-          console.log("Hmm. They're not equal?");
-          return Boolean(false);
-        }
+        // this means they are either both arrays, and thus obj1 == obj2, or obj1 and obj2 are the
+        // wrong data types altogether, in which case, they are not equal.
+        console.log("Hmm. They're not equal? (3)"); //debug only
+        return Boolean(false);
       }
     }
   }
