52d3da00a9ebbbcd0d7fefabc3d5a8c853d1232e
Noah Tatko <ntat.taco@gmail.com>
2018-03-14 08:39:11 -0500

    Worked on exercise 4-4. Still wondering exactly what it means by 'deep equal'. I suppose I'll ask that tonight.

--- exercise 4-4.js
+++ exercise 4-4.js
@@ -6,18 +6,51 @@ function deepEqual(obj1, obj2) {
       // obj1 is a list
       if (obj2.value) {
         // obj2 is a list
-
+        let compared = Boolean(true);
+        while (obj1.rest != null && obj2.rest != null) {
+          if (obj1.value != obj2.value) {
+            compared = Boolean(false);
+          }
+          obj1 = obj1.rest;
+          obj2 = obj2.rest;
+        }
+        return compared;
       } else {
         // obj2 is a list
-
+        if (Array.isArray(obj2)) {
+          let compared = Boolean(true);
+          for (let i = 0; i < obj2.length; i++) {
+            if (obj1.value != obj2[i]) {
+              compared = Boolean(false);
+            }
+            obj1 = obj1.value;
+          }
+          return compared;
+        } else {
+          console.log("Hmm. They're not equal?");
+          return Boolean(false);
+        }
       }
     } else {
       if (obj2.value) {
         // obj2 is a list
+        if (Array.isArray(obj1)) {
+          for (let i = 0; i < obj1.length; i++) {
+
+          }
+        } else {
+          console.log("Hmm. They're not equal?");
+          return Boolean(false);
+        }
 
       } else {
         // obj2 is not a list
+        if (Array.isArray(obj2)){
 
+        } else {
+          console.log("Hmm. They're not equal?");
+          return Boolean(false);
+        }
       }
     }
   }
