788c8ec1a5bdeb05ad1fdfda50c1bbd61a73efcd
Noah Tatko <ntat.taco@gmail.com>
2018-03-14 19:26:35 -0500

    new version that actually works

--- exercise 2-3.js
+++ exercise 2-3.js
@@ -1,13 +1,14 @@
 function chessboard(size = 8, character = "#") {
   for (let i = 0; i < size; i++) {
     let str = ''
-    for (let j = 0; j < size; i++) {
-      if ((i + j) % 2 == 0) {
+    for (let j = 0; j < size; j++) {
+      if ( (i + j) % 2 == 0) {
         str += ' ';
       } else {
         str += character;
       }
     }
+    console.log(str);
   }
 }
 
